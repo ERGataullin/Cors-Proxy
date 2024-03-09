c1315b5daf60d1b09d0bf03d4687ec374b91266a
Edgar Gataullin <ERGataullin2000@gmail.com>
2024-03-09 23:19:42 +0300

    Shelf main

--- bin/shelf_main.dart
+++ bin/shelf_main.dart
@@ -1,16 +1,20 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+final http.Client _client = http.Client();
+
 void main() async {
-  var handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  final FutureOr<Response> Function(Request) handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_handleRequest);
 
   final SecurityContext securityContext = SecurityContext.defaultContext
-    ..useCertificateChain(r'')
-    ..usePrivateKey(r'');
-  var server = await shelf_io.serve(
+    ..useCertificateChain(r'/root/certificate/zerossl_certificate.crt')
+    ..usePrivateKey(r'/root/certificate/private.key');
+  final HttpServer server = await shelf_io.serve(
     handler,
     InternetAddress.anyIPv4,
     443,
@@ -23,5 +27,20 @@ void main() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-Response _echoRequest(Request request) =>
-    Response.ok('Request for "${request.url}"');
+Future<Response> _handleRequest(Request request) async {
+  final Uri remoteUri = Uri.parse(
+    Uri.decodeComponent(request.url.path),
+  ).replace(
+    query: request.url.hasQuery ? request.url.query : null,
+    fragment: request.url.hasFragment ? request.url.fragment : null,
+  );
+  final http.BaseRequest remoteRequest = http.Request(request.method, remoteUri)
+    ..followRedirects = false;
+
+  final remoteResponse = await _client.send(remoteRequest);
+
+  return Response.ok(
+    remoteResponse.stream,
+    headers: remoteResponse.headersSplitValues,
+  );
+}
