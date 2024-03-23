7a0c17709deccc738dde767895fbdc29913aef75
Edgar Gataullin <ERGataullin2000@gmail.com>
2024-03-23 15:52:47 +0300

    Working on shelf implementation

--- bin/shelf_main.dart
+++ bin/shelf_main.dart
@@ -4,12 +4,15 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_proxy/shelf_proxy.dart';
 
 final http.Client _client = http.Client();
 
 void main() async {
-  final FutureOr<Response> Function(Request) handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(_handleRequest);
+  final FutureOr<Response> Function(Request) handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(_mapKakiHeader())
+      .addHandler(_handleRequest);
 
   final SecurityContext securityContext = SecurityContext.defaultContext
     ..useCertificateChain(r'/root/certificate/zerossl_certificate.crt')
@@ -17,7 +20,7 @@ void main() async {
   final HttpServer server = await shelf_io.serve(
     handler,
     InternetAddress.anyIPv4,
-    443,
+    8080,
     securityContext: securityContext,
   );
 
@@ -27,20 +30,45 @@ void main() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
+Middleware _mapKakiHeader() {
+  return (Handler handler) {
+    return (Request request) async {
+      final Request mappedRequest = request.change(
+        headers: {
+          for (final String headerName in request.headers.keys)
+            headerName == 'kaki' ? 'cookie' : headerName:
+                request.headers[headerName],
+        },
+      );
+      final Response originalResponse = await handler(mappedRequest);
+
+      return originalResponse.change(
+        headers: {
+          for (final String headerName in request.headers.keys)
+            headerName == 'set-cookie' ? 'set-kaki' : headerName:
+                request.headers[headerName],
+        },
+      );
+    };
+  };
+}
+
 Future<Response> _handleRequest(Request request) async {
-  final Uri remoteUri = Uri.parse(
+  Uri? uri = Uri.tryParse(
     Uri.decodeComponent(request.url.path),
-  ).replace(
-    query: request.url.hasQuery ? request.url.query : null,
-    fragment: request.url.hasFragment ? request.url.fragment : null,
   );
-  final http.BaseRequest remoteRequest = http.Request(request.method, remoteUri)
-    ..followRedirects = false;
 
-  final remoteResponse = await _client.send(remoteRequest);
+  if (uri != null && uri.hasScheme && uri.hasAuthority) {
+    uri = uri.replace(
+      query: request.url.hasQuery ? request.url.query : null,
+      fragment: request.url.hasFragment ? request.url.fragment : null,
+    );
+  } else {
+    return Response.badRequest(body: 'No URL provided.');
+  }
 
-  return Response.ok(
-    remoteResponse.stream,
-    headers: remoteResponse.headersSplitValues,
-  );
+  return await proxyHandler(
+    uri,
+    client: _client,
+  )(request);
 }
